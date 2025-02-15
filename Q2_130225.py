77f287f97f863dce21da1f017a09abffc47557b2
samarthraj <samarthl.raj@gmail.com>
2025-02-15 09:55:15 -0600

    150225 commit

--- Q2_130225.py
+++ Q2_130225.py
@@ -2,16 +2,6 @@
 def remove_duplicates(nums): 
 
 #    if not nums:
-    #     return 0
-
-
-    # index = 0
-    # for i in range(1, len(nums)): 
-    #     if nums[index] != nums[i]:
-    #         index += 1
-    #         nums[index] = nums[i] 
-    
-    # return index + 1
 
     index = 0
     for i in range(0, len(nums)): 
